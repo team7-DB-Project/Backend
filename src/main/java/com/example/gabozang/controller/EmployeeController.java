c5b1ebf6acb4644e87ab5c1ea4b9e1e006a2b788
ChoiYeongChan <supaul97@naver.com>
2024-02-15 08:50:10 +0900

    feat/ employee, payment_history, review 생성 및 조회

--- src/main/java/com/example/gabozang/controller/EmployeeController.java
+++ src/main/java/com/example/gabozang/controller/EmployeeController.java
@@ -1,7 +1,32 @@
 package com.example.gabozang.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.gabozang.domain.employee.Dto.EmployeeRequestDto;
+import com.example.gabozang.domain.employee.Dto.EmployeeResponseDto;
+import com.example.gabozang.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @PostMapping("/employee")
+    public ResponseEntity<String> InsertEmployee(@RequestBody EmployeeRequestDto.EmployeeReqInfo employeeReqInfo) throws IOException {
+        return ResponseEntity.ok(employeeService.insertEmployee(employeeReqInfo));
+    }
+
+    @GetMapping("/employee")
+    public ResponseEntity<List<EmployeeResponseDto.EmployeeResInfo>> SelectAllEmployee() throws IOException {
+        return ResponseEntity.ok(employeeService.selectAllEmployee());
+    }
+
+    @GetMapping("/employee/{employeeId}")
+    public ResponseEntity<EmployeeResponseDto.EmployeeResInfo> SelectEmployeeById(@PathVariable int employeeId) throws IOException {
+        return ResponseEntity.ok(employeeService.selectEmployeeById(employeeId));
+    }
 }
--- src/main/java/com/example/gabozang/service/ReviewService.java
+++ src/main/java/com/example/gabozang/service/ReviewService.java
@@ -1,9 +1,27 @@
 package com.example.gabozang.service;
 
+import com.example.gabozang.domain.review.Dto.ReviewRequestDto.ReviewReqInfo;
+import com.example.gabozang.domain.review.Dto.ReviewResponseDto.ReviewResInfo;
+import com.example.gabozang.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
+    private final ReviewRepository reviewRepository;
+
+    public String insertReview(ReviewReqInfo reviewReqInfo) {
+        return reviewRepository.insertReview(reviewReqInfo);
+    }
+
+    public List<ReviewResInfo> selectAllReview() {
+        return reviewRepository.selectAllReview();
+    }
+
+    public ReviewResInfo selectReviewById(int reviewId) {
+        return reviewRepository.selectReviewById(reviewId);
+    }
 }
