dd0e571001093354db46cfdd2257df42add74355
최영찬 <76672161+ChoiYeongChan@users.noreply.github.com>
2024-02-15 08:54:57 +0900

    Feat/choiyeongchan (#4)
    * feat/ employee, payment_history, review 생성 및 조회
    
    * feat/ employee, payment_history, review 생성 및 조회

---
+++ src/main/java/com/example/gabozang/domain/review/Dto/ReviewResponseDto.java
@@ -0,0 +1,21 @@
+package com.example.gabozang.domain.review.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+public class ReviewResponseDto {
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewResInfo {
+        private int reviewId;
+        private int storeId;
+        private String content;
+        private double rating;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+    }
+}
