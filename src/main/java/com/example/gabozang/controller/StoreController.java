1c40f1a8ccbc141e47666d49caf1c08470747e29
SewoongPark <lympsw12@naver.com>
2024-02-14 13:59:10 +0900

    feat/ commit1

--- src/main/java/com/example/gabozang/controller/StoreController.java
+++ src/main/java/com/example/gabozang/controller/StoreController.java
@@ -1,13 +1,14 @@
 package com.example.gabozang.controller;
 
-import com.example.gabozang.domain.store.Dto.StoreRequestDto.StoreInfo;
+import com.example.gabozang.domain.store.Dto.StoreRequestDto.StoreReqInfo;
+import com.example.gabozang.domain.store.Dto.StoreResponseDto.StoreReSInfo;
 import com.example.gabozang.service.StoreService;
 import java.io.IOException;
+import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,8 +19,30 @@ public class StoreController {
      * Store 저장
      */
     @PostMapping("/store")
-    public ResponseEntity<String> EatMedicine(@RequestBody StoreInfo storeInfo) throws IOException {
-        return ResponseEntity.ok(storeService.insertStore(storeInfo));
+    public ResponseEntity<String> InsertStore(@RequestBody StoreReqInfo storeReqInfo) throws IOException {
+        return ResponseEntity.ok(storeService.insertStore(storeReqInfo));
     }
+
+    /**
+     * 모든 Store 조회 (이름, 위치, 총 매출)
+     */
+    @GetMapping("/store")
+    public ResponseEntity<List<StoreReSInfo>> SelectAllStore() throws IOException {
+        return ResponseEntity.ok(storeService.selectAllStore());
+    }
+
+    /**
+     *  Id로 특정 Store 조회
+     */
+    @GetMapping("/store/{storeId}")
+    public ResponseEntity<StoreReSInfo> SelectStoreById(@PathVariable int storeId) throws IOException {
+        return ResponseEntity.ok(storeService.selectStoreById(storeId));
+    }
+
+    /**
+     * 매출 순위 Top 3 조회
+     */
+
+    /*asd*/
 
 }
--- src/main/java/com/example/gabozang/repository/StoreRepository.java
+++ src/main/java/com/example/gabozang/repository/StoreRepository.java
@@ -1,12 +1,13 @@
 package com.example.gabozang.repository;
 
-import com.example.gabozang.domain.store.Dto.StoreRequestDto.StoreInfo;
+import com.example.gabozang.domain.store.Dto.StoreRequestDto.StoreReqInfo;
+import com.example.gabozang.domain.store.Dto.StoreResponseDto.StoreReSInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @Repository
 public class StoreRepository {
@@ -18,11 +19,43 @@ public class StoreRepository {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public String insertStore(StoreInfo storeInfo){
-        String createUserQuery = "insert into store (name, location, maximum_capacity, rating,image_url, phone_number, total_sales) VALUES (?,?,?,?,?,?,?)"; // 실행될 동적 쿼리문
-        Object[] createUserParams = new Object[]{storeInfo.getName(), storeInfo.getLocation(), storeInfo.getMaximumCapacity(), storeInfo.getRating(), storeInfo.getImageUrl(),
-                storeInfo.getPhoneNumber(), storeInfo.getTotalSales()};
-        this.jdbcTemplate.update(createUserQuery, createUserParams);
+    public String insertStore(StoreReqInfo storeReqInfo){
+        String createStoreQuery = "insert into store (name, location, maximum_capacity, rating,image_url, phone_number, total_sales) VALUES (?,?,?,?,?,?,?)"; // 실행될 동적 쿼리문
+        Object[] createUserParams = new Object[]{storeReqInfo.getName(), storeReqInfo.getLocation(), storeReqInfo.getMaximumCapacity(), storeReqInfo.getRating(), storeReqInfo.getImageUrl(),
+                storeReqInfo.getPhoneNumber(), storeReqInfo.getTotalSales()};
+        this.jdbcTemplate.update(createStoreQuery, createUserParams);
         return "점포 정보 저장 완료";
     }
+
+    public List<StoreReSInfo> selectAllStore() {
+        String selectStoreQuery = "select * from store";
+        return this.jdbcTemplate.query(selectStoreQuery,
+                (rs, rowNum) -> new StoreReSInfo(
+                        rs.getInt("store_id"),
+                        rs.getString("name"),
+                        rs.getString("location"),
+                        rs.getInt("maximum_capacity"),
+                        rs.getDouble("rating"),
+                        rs.getString("image_url"),
+                        rs.getString("phone_number"),
+                        rs.getInt("total_sales")
+                ));
+    }
+
+    public StoreReSInfo selectStoreById(int store_id) {
+        String selectStoreQuery = "select * from store where store_id = ?";
+        return this.jdbcTemplate.queryForObject(selectStoreQuery,
+                (rs, rowNum) -> new StoreReSInfo(
+                        rs.getInt("store_id"),
+                        rs.getString("name"),
+                        rs.getString("location"),
+                        rs.getInt("maximum_capacity"),
+                        rs.getDouble("rating"),
+                        rs.getString("image_url"),
+                        rs.getString("phone_number"),
+                        rs.getInt("total_sales")),
+                    store_id
+                );
+    }
+
 }
