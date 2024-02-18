03dca9ee62a6bfcdb394f3c4c187b910da95fd18
SewoongPark <lympsw12@naver.com>
2024-02-18 18:28:50 +0900

    Feat/sewoong (#6)
    * feat/ store모두 조회 및 특정 id로 조회
    
    * feat/ store update, store top 3

--- src/main/java/com/example/gabozang/controller/StoreController.java
+++ src/main/java/com/example/gabozang/controller/StoreController.java
@@ -1,7 +1,7 @@
 package com.example.gabozang.controller;
 
 import com.example.gabozang.domain.store.Dto.StoreRequestDto.StoreReqInfo;
-import com.example.gabozang.domain.store.Dto.StoreResponseDto.StoreReSInfo;
+import com.example.gabozang.domain.store.Dto.StoreResponseDto.StoreResInfo;
 import com.example.gabozang.service.StoreService;
 import java.io.IOException;
 import java.util.List;
@@ -28,7 +28,7 @@ public class StoreController {
      * 모든 Store 조회 (이름, 위치, 총 매출)
      */
     @GetMapping("/store")
-    public ResponseEntity<List<StoreReSInfo>> SelectAllStore() throws IOException {
+    public ResponseEntity<List<StoreResInfo>> SelectAllStore() throws IOException {
         return ResponseEntity.ok(storeService.selectAllStore());
     }
 
@@ -36,13 +36,26 @@ public class StoreController {
      *  Id로 특정 Store 조회
      */
     @GetMapping("/store/{storeId}")
-    public ResponseEntity<StoreReSInfo> SelectStoreById(@PathVariable int storeId) throws IOException {
+    public ResponseEntity<StoreResInfo> SelectStoreById(@PathVariable int storeId) throws IOException {
         return ResponseEntity.ok(storeService.selectStoreById(storeId));
     }
 
     /**
-     * 매출 순위 Top 3 조회
+     * 업데이트
      */
+    @PostMapping("/store/{storeId}")
+    public ResponseEntity<Integer> updateStoreById(@PathVariable int storeId, @RequestBody StoreReqInfo storeReqInfo) {
+        return ResponseEntity.ok(storeService.updateStoreById(storeId, storeReqInfo));
+    }
+
+    /**
+     * Top3 출력
+     */
+    @GetMapping("/store/top3")
+    public ResponseEntity<List<StoreResInfo>> selectStoreByIdTop3() {
+        return ResponseEntity.ok(storeService.selectAllStoreTop3());
+    }
+
 
 
 
--- src/main/java/com/example/gabozang/repository/StoreRepository.java
+++ src/main/java/com/example/gabozang/repository/StoreRepository.java
@@ -1,7 +1,8 @@
 package com.example.gabozang.repository;
 
 import com.example.gabozang.domain.store.Dto.StoreRequestDto.StoreReqInfo;
-import com.example.gabozang.domain.store.Dto.StoreResponseDto.StoreReSInfo;
+import com.example.gabozang.domain.store.Dto.StoreResponseDto.StoreResInfo;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -20,42 +21,65 @@ public class StoreRepository {
     }
 
     public String insertStore(StoreReqInfo storeReqInfo){
-        String createStoreQuery = "insert into store (name, location, maximum_capacity, rating,image_url, phone_number, total_sales) VALUES (?,?,?,?,?,?,?)"; // 실행될 동적 쿼리문
+        String createStoreQuery = "insert into store (name, location, maximum_capacity, rating,image_url, phone_number) VALUES (?,?,?,?,?,?)"; // 실행될 동적 쿼리문
         Object[] createUserParams = new Object[]{storeReqInfo.getName(), storeReqInfo.getLocation(), storeReqInfo.getMaximumCapacity(), storeReqInfo.getRating(), storeReqInfo.getImageUrl(),
-                storeReqInfo.getPhoneNumber(), storeReqInfo.getTotalSales()};
+                storeReqInfo.getPhoneNumber()};
         this.jdbcTemplate.update(createStoreQuery, createUserParams);
         return "점포 정보 저장 완료";
     }
 
-    public List<StoreReSInfo> selectAllStore() {
+    public List<StoreResInfo> selectAllStore() {
         String selectStoreQuery = "select * from store";
         return this.jdbcTemplate.query(selectStoreQuery,
-                (rs, rowNum) -> new StoreReSInfo(
+                (rs, rowNum) -> new StoreResInfo(
                         rs.getInt("store_id"),
                         rs.getString("name"),
                         rs.getString("location"),
                         rs.getInt("maximum_capacity"),
                         rs.getDouble("rating"),
                         rs.getString("image_url"),
-                        rs.getString("phone_number"),
-                        rs.getInt("total_sales")
+                        rs.getString("phone_number")
                 ));
     }
 
-    public StoreReSInfo selectStoreById(int store_id) {
+    public StoreResInfo selectStoreById(int store_id) {
         String selectStoreQuery = "select * from store where store_id = ?";
         return this.jdbcTemplate.queryForObject(selectStoreQuery,
-                (rs, rowNum) -> new StoreReSInfo(
+                (rs, rowNum) -> new StoreResInfo(
                         rs.getInt("store_id"),
                         rs.getString("name"),
                         rs.getString("location"),
                         rs.getInt("maximum_capacity"),
                         rs.getDouble("rating"),
                         rs.getString("image_url"),
-                        rs.getString("phone_number"),
-                        rs.getInt("total_sales")),
+                        rs.getString("phone_number")),
                     store_id
                 );
+    }
+
+    @Transactional
+    public int updateStoreInfo(int storeId, StoreReqInfo storeReqInfo) {
+        String updateStoreQuery = "update store set name = ?, location = ?, maximum_capacity = ?, rating = ?, image_url = ?," +
+                " phone_number = ? where store_id = ?";
+        Object[] modifyStoreIdParams = new Object[]{storeReqInfo.getName(), storeReqInfo.getLocation(),
+                storeReqInfo.getMaximumCapacity(), storeReqInfo.getRating(), storeReqInfo.getImageUrl(),
+                storeReqInfo.getPhoneNumber(), storeId};
+
+        return this.jdbcTemplate.update(updateStoreQuery, modifyStoreIdParams);
+    }
+
+    public List<StoreResInfo> selectStoreTop3() {
+        String selectStoreQuery = "select * from store order by rating desc limit 3";
+        return this.jdbcTemplate.query(selectStoreQuery,
+                (rs, rowNum) -> new StoreResInfo(
+                        rs.getInt("store_id"),
+                        rs.getString("name"),
+                        rs.getString("location"),
+                        rs.getInt("maximum_capacity"),
+                        rs.getDouble("rating"),
+                        rs.getString("image_url"),
+                        rs.getString("phone_number")
+                ));
     }
 
 }
